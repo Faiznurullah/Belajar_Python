7fee7cd08c1aa59880424628df962b774700d441
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2022-05-31 19:57:41 +0700

    create conversion file

---
+++ src/conversion.py
@@ -0,0 +1,12 @@
+
+    print(float(5))
+    print(int(10.6))
+    print(int(-10.6))
+    print(float('2.5'))
+    print(str(25))
+    print(int('1p'))
+    print(set([1,2,3]))
+    print(tuple({5,6,7}))
+    print(list('hello'))
+    print(dict([[1,2],[3,4]]))
+    print(dict([(3,26),(4,44)]))
