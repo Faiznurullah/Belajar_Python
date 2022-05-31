5cb6634c6de857b77ed6a4fd17ff129bdc6ec89b
Grakody <59213454+Faiznurullah@users.noreply.github.com>
2022-05-31 19:54:01 +0700

    create dictionary file

---
+++ src/dictionary.py
@@ -0,0 +1,14 @@
+
+
+    d = {1:'value','key':2}
+    print(type(d))
+    
+    //
+    
+    d = {1:'value','key':2}
+    print(type(d))
+    print("d[1] = ", d[1]);
+    print("d['key'] = ", d['key']);
+    
+    
+    
